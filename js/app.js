9bb826e01e2aa58e4d7ecfe88a5b9409edb08b2b
edgardorito <edgardorito@gmail.com>
2018-02-20 18:45:53 -0600

    Modificando elementos

--- js/app.js
+++ js/app.js
@@ -1,2 +1,9 @@
-let saludo = "Hola mundo desde un archivo js";
-console.log(saludo);
+//const lista = document.getElementsByTagName('collection-item')
+const lista = document.getElementsByTagName('li');
+for (var i = 0; i < lista.length; i++) {
+  if (i % 2 == 0) {
+    let element = lista[i];
+    element.style.background = '#f2f2f2'
+    //console.log(element);
+  }
+}
